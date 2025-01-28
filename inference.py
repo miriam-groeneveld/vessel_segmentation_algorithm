51ac96f889e72435a93363c6e99b5ace590f965d
miriam-groeneveld <51329283+miriam-groeneveld@users.noreply.github.com>
2025-01-28 22:19:31 +0100

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,107 @@
+"""
+The following is a simple algorithm template that matches the configuration for your algorithm.
+
+It is meant to run within a container.
+
+To run it locally, you can call the following bash script:
+
+  ./do_test_run.sh
+
+This will start the inference and reads from ./test/input and outputs to ./test/output
+
+To save the container and prep it for upload to Grand-Challenge.org you can call:
+
+  ./do_save.sh
+
+Any container that shows the same behavior will do, this is purely an example of how one COULD do it.
+
+Happy programming!
+"""
+from pathlib import Path
+import json
+from glob import glob
+import SimpleITK
+import numpy
+
+INPUT_PATH = Path("/input")
+OUTPUT_PATH = Path("/output")
+RESOURCE_PATH = Path("resources")
+
+
+def run():
+    # Read the input
+    input_oct_image = load_image_file_as_array(
+        location=INPUT_PATH / "images/oct",
+    )
+    input_age_in_months = load_json_file(
+        location=INPUT_PATH / "age-in-months.json",
+    )
+
+    # Process the inputs: any way you'd like
+    _show_torch_cuda_info()
+
+    with open(RESOURCE_PATH / "some_resource.txt", "r") as f:
+        print(f.read())
+
+    # TODO: add your custom inference here
+
+    # For now, let us make bogus predictions
+    output_retinal_vessel_segmentation = numpy.eye(4, 2)
+
+    # Save your output
+    write_array_as_image_file(
+        location=OUTPUT_PATH / "images/vessel-segmentation",
+        array=output_retinal_vessel_segmentation,
+    )
+
+    return 0
+
+
+def load_json_file(*, location):
+    # Reads a json file
+    with open(location, "r") as f:
+        return json.loads(f.read())
+
+
+def load_image_file_as_array(*, location):
+    # Use SimpleITK to read a file
+    input_files = (
+        glob(str(location / "*.tif"))
+        + glob(str(location / "*.tiff"))
+        + glob(str(location / "*.mha"))
+    )
+    result = SimpleITK.ReadImage(input_files[0])
+
+    # Convert it to a Numpy array
+    return SimpleITK.GetArrayFromImage(result)
+
+
+def write_array_as_image_file(*, location, array):
+    location.mkdir(parents=True, exist_ok=True)
+
+    # You may need to change the suffix to .tif to match the expected output
+    suffix = ".mha"
+
+    image = SimpleITK.GetImageFromArray(array)
+    SimpleITK.WriteImage(
+        image,
+        location / f"output{suffix}",
+        useCompression=True,
+    )
+
+
+def _show_torch_cuda_info():
+    import torch
+
+    print("=+=" * 10)
+    print("Collecting Torch CUDA information")
+    print(f"Torch CUDA is available: {(available := torch.cuda.is_available())}")
+    if available:
+        print(f"\tnumber of devices: {torch.cuda.device_count()}")
+        print(f"\tcurrent device: { (current_device := torch.cuda.current_device())}")
+        print(f"\tproperties: {torch.cuda.get_device_properties(current_device)}")
+    print("=+=" * 10)
+
+
+if __name__ == "__main__":
+    raise SystemExit(run())
