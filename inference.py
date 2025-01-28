5aee4eac55b34de4190e5e2c612f5cf470aa753a
miriam-groeneveld <51329283+miriam-groeneveld@users.noreply.github.com>
2025-01-28 22:37:40 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -22,6 +22,11 @@ import json
 from glob import glob
 import SimpleITK
 import numpy
+import torch
+import monai
+
+from scipy.special import expit
+from skimage import transform
 
 INPUT_PATH = Path("/input")
 OUTPUT_PATH = Path("/output")
@@ -39,21 +44,61 @@ def run():
 
     # Process the inputs: any way you'd like
     _show_torch_cuda_info()
-
+    # use GPU if available, otherwise use the CPU
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  
     with open(RESOURCE_PATH / "some_resource.txt", "r") as f:
         print(f.read())
 
     # TODO: add your custom inference here
+    # define the U-Net with monai
+    self.model = monai.networks.nets.UNet(
+        dimensions=2,
+        in_channels=3,
+        out_channels=1,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+    ).to(self.device)
+    print(f'==> Model created')
 
-    # For now, let us make bogus predictions
-    output_retinal_vessel_segmentation = numpy.eye(4, 2)
+    res = self.model.eval()  # set to inference mode
+    print(f'==> Model evaluated')
 
+    self.model.load_state_dict(
+        torch.load(
+            "./best_metric_model_segmentation2d_dict.pth",
+            map_location=self.device,
+        )
+    )
+
+    print("==> Weights loaded")
+   
+    image = SimpleITK.GetArrayFromImage(input_image)
+    image = np.array(image)
+    shape = image.shape
+
+    # Pre-process the image
+    image = transform.resize(image, (512, 512), order=3)  # resize all images to 512 x 512 in shape
+    image = image.astype(np.float32) / 255.  # normalize
+    image = image.transpose((2, 0, 1))  # flip the axes and bring color to the first channel
+    image = torch.from_numpy(image).to(self.device).reshape(1, 3, 512, 512)
+
+    # Do the forward pass
+    out = self.model(image).squeeze().data.cpu().numpy()
+
+    # Post-process the image
+    out = transform.resize(out, shape[:-1], order=3)
+    out = (expit(out) > 0.99)  # apply the sigmoid filter and binarize the predictions
+    out = (out * 255).astype(np.uint8)
+    out = SimpleITK.GetImageFromArray(out)  # convert numpy array to SimpleITK image for grand-challenge.org    
+    
     # Save your output
     write_array_as_image_file(
         location=OUTPUT_PATH / "images/vessel-segmentation",
-        array=output_retinal_vessel_segmentation,
+        array=out,
     )
-
+    print("==> Prediction done")
     return 0
 
 
